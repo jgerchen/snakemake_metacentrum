b4664a4e7ad767bb6eb021bd816bc805a6cb667b
Jorn Frederik Gerchen <gerchenj@skirit.ics.muni.cz>
2023-04-05 11:12:03 +0200

    First version that actually works

--- status.py
+++ status.py
@@ -1,15 +1,24 @@
-
+#!/usr/bin/env python3
+import subprocess
 import sys
+import re
 jobid = sys.argv[1]
 
-
-
-
-
-
-
-print("running")
-
-print("failed")
-
-print("success")
+job_status_cmd="qstat -xf %s" % jobid
+job_status_reply=subprocess.run(job_status_cmd, check=True, shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
+run_status=re.findall("job_state = (.)", job_status_reply)[0]
+if run_status=="M":
+	moved_queue=re.findall("queue = .+@(.+)", job_status_reply)[0]
+	job_status_cmd_mvd="qstat -xf %s@%s" % (jobid, moved_queue)
+	job_status_reply=subprocess.run(job_status_cmd_mvd, check=True, shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
+	run_status=re.findall("job_state = (.)", job_status_reply)[0]
+if run_status=="R" or run_status=="Q":
+	print("running")
+elif run_status=="F":
+	exit_status=re.findall("Exit_status = (.+)", job_status_reply)[0]
+	if exit_status=="0":
+		print("success")
+	else:
+		print("failed")
+else:
+	print("Error: unable to get job status for job %s" % jobid)
--- submit.py
+++ submit.py
@@ -6,11 +6,11 @@ from snakemake.utils import read_job_properties
 
 jobscript=sys.argv[1]
 job_probs=read_job_properties(jobscript)
-submit_command="qsub %s -l select=1:ncpus=%s:mem=%smb:scratch_local=%smb -l walltime=%s -m n -o %s.cluster.o -e %s.cluster.e" % (jobscript, job_probs["threads"], job_probs["resources"]["mem_mb"], job_probs["resources"]["disk_mb"], job_probs["resources"]["runtime"], job_probs["log"][0], job_probs["log"][0])
-submit.response=subprocess.run(submit_command, check=True, shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
-#TODO: get job id from output using RE
-job_id=^[0-9]+\.
-#TODO: change the name of error and output files job was submitted and we have a jobid
+submit_command="qsub -l select=1:ncpus=%s:mem=%smb:scratch_local=%smb -l walltime=%s -m n -o %s.cluster.o -e %s.cluster.e %s" % (job_probs["threads"], job_probs["resources"]["mem_mb"], job_probs["resources"]["disk_mb"], job_probs["resources"]["runtime"], job_probs["log"][0], job_probs["log"][0], jobscript)
+submit_response=subprocess.run(submit_command, check=True, shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
+#get job id from output using RE
+job_id=re.findall("^[0-9]+", submit_response)[0]
+#change the name of error and output files job was submitted and we have a jobid
 #qalter -o ${path}.o{$jobid} -e ${path}.e${jobid} 
 change_output_response=subprocess.run("qalter -o %s.o%s -e %s.e%s %s" % (job_probs["log"][0], job_id, job_probs["log"][0], job_id, job_id), check=True, shell=True)
 
