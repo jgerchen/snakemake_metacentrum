56b4dc33e78242647ef292d0599002b682d7ae1c
Jorn Frederik Gerchen <gerchenj@skirit.ics.muni.cz>
2023-04-05 11:34:53 +0200

    Now it works.

--- status.py
+++ status.py
@@ -12,7 +12,7 @@ if run_status=="M":
 	job_status_cmd_mvd="qstat -xf %s@%s" % (jobid, moved_queue)
 	job_status_reply=subprocess.run(job_status_cmd_mvd, check=True, shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
 	run_status=re.findall("job_state = (.)", job_status_reply)[0]
-if run_status=="R" or run_status=="Q":
+if run_status=="R" or run_status=="Q" or run_status=="E":
 	print("running")
 elif run_status=="F":
 	exit_status=re.findall("Exit_status = (.+)", job_status_reply)[0]
